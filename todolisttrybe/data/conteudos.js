1027cb022b39eb38b642132941311fcf0158361c
enicio <jessandro42@gmail.com>
2020-11-22 16:54:35 -0300

    Adicionado links dos conteudos

---
+++ todolisttrybe/data/conteudos.js
@@ -0,0 +1,203 @@
+const kombiCarregada =
+  ['Setup',
+    'Fundamentos do Desenvolvimento Web',
+    'Introdução - Unix & Shell',
+    'Unix & Bash - Parte 1',
+    'Unix & Bash - Parte 2'
+    , ,
+    'Git & GitHub - O que é e para que serve',
+    'Git & GitHub - Entendendo os comandos',
+    'Internet - Entendendo como ela funciona'
+    , ,
+    'Introdução - HTML & CSS',
+    'HTML & CSS - Estruturas de página',
+    'HTML & CSS - Primeiros passos em CSS',
+    'HTML & CSS - Seletores e posicionamento',
+    'HTML Semântico',
+    'Projeto - Lessons Learned'
+    , ,
+    'Introdução - JavaScript',
+    'JavaScript - Primeiros passos',
+    'JavaScript - Array e loop For',
+    'JavaScript - Lógica de Programação e Algoritmos',
+    'JavaScript - Objetos e funções',
+    'Projeto - Playground Functions'
+    , ,
+    'JavaScript - DOM e seletores',
+    'JavaScript - Trabalhando com elementos',
+    'JavaScript - Eventos',
+    'JavaScript - Web Storage',
+    'Fundamentos - JavaScript - Projetos',
+    'Projeto - Arte com Pixels',
+    'Projeto - Lista de tarefas',
+    '(Bônus) Projeto - Meme Generator',
+    '(Bônus) Projeto - Adivinhe a Cor',
+    '(Bônus) Projeto - Carta Misteriosa'
+    , ,
+    'HTML & CSS - Forms',
+    'Bibliotecas JavaScript e Frameworks CSS',
+    'Introdução - CSS Flexbox',
+    'CSS Flexbox - Parte 1',
+    'CSS Flexbox - Parte 2',
+    'CSS Responsivo - Mobile First',
+    'Projeto - Página inicial do Facebook'
+    , ,
+    'JavaScript ES6 - let, const, arrow functions e template literals',
+    'JavaScript ES6 - Objects',
+    'Testes unitários em JavaScript',
+    'Projeto - JavaScript Testes Unitários'
+    , ,
+    'JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort',
+    'JavaScript ES6 - Higher Order Functions - map e filter',
+    'JavaScript ES6 - Higher Order Functions - reduce',
+    'JavaScript ES6 - spread operator, parâmetro rest, destructuring e mais',
+    'Projeto - Zoo functions'
+    , ,
+    'JavaScript Assíncrono e Callbacks',
+    'JavaScript Promises',
+    'Projeto - Carrinho de Compras'
+    , ,
+    'Primeiros passos no Jest',
+    'Jest - Testes Assíncronos',
+    'Jest - Simulando comportamentos',
+    'Projeto - Jest Assíncrono e Mocking'
+    , ,
+    'Introdução - Front-end',
+    'Introdução - React',
+    'Hello, world! no React!',
+    'Componentes React',
+    'Projeto - Movie Cards Library'
+    , ,
+    'Componentes com estado e eventos',
+    'Formulários no React',
+    'Projeto - Movie Cards Library Stateful'
+    , ,
+    'Ciclo de vida de componentes',
+    'React Router',
+    'Projeto - Movie Cards Library CRUD'
+    , ,
+    'Metodologias Ágeis',
+    'Projeto - Frontend Online Store'
+    , ,
+    'RTL - Primeiros passos',
+    'RTL - Mocks e Inputs',
+    'RTL - Testando React Router',
+    'Projeto - Testes em React'
+    , ,
+    'Introdução ao Redux - O estado global da aplicação',
+    'Usando o Redux no React',
+    'Usando o Redux no React - Prática',
+    'Usando o Redux no React - Actions Assíncronas',
+    'Testes em React-Redux',
+    'Projeto - Trybe Wallet'
+    , ,
+    'Projeto - Jogo de Trivia'
+    , ,
+    'Context API do React',
+    'React Hooks - useState e useContext',
+    'React Hooks - useEffect e Hooks customizados',
+    'Projeto - StarWars Datatable com Context API e Hooks'
+    , ,
+    'Projeto - App de Receitas'
+    , ,
+    'Introdução - Back-end',
+    'Introdução - Bancos de dados relacionais',
+    'Banco de dados SQL',
+    'Encontrando dados em um banco de dados',
+    'Filtrando dados de forma específica',
+    'Manipulando tabelas',
+    'Projeto - All For One'
+    , ,
+    'Funções mais usadas no SQL',
+    'Descomplicando JOINs e UNIONs',
+    'Stored Routines & Subqueries',
+    'Projeto - Vocabulary Booster'
+    , ,
+    'Transformando ideias em um modelo de banco de dados',
+    'Normalização Formas Normais e Dumps',
+    'Transformando ideias em um modelo de banco de dados - Parte 2',
+    'Projeto - One For All'
+    , ,
+    'Introdução - NoSQL',
+    'MongoDB - Introdução',
+    'Filter Operators',
+    'Projeto - Data Flights'
+    , ,
+    'Updates Simples',
+    'Updates Complexos - Arrays - Parte 1',
+    'Updates Complexos - Arrays - Parte 2',
+    'Projeto - Commerce'
+    , ,
+    'Aggregation Framework - Parte 1',
+    'Aggregation Framework - Parte 2',
+    'Projeto - Aggregations'
+    , ,
+    'Intro - NodeJS',
+    'NodeJS - Introdução',
+    'NodeJS - Fluxo Assíncrono',
+    'NodeJS - Arquitetura',
+    'Express: HTTP com Node.js',
+    'Práticando Express',
+    'Projeto - Crush Manager'
+    , ,
+    'Introdução - Arquitetura de Software',
+    'Arquitetura de Software - Camada de Model',
+    'Arquitetura de Software - Camada de Controller e Service',
+    'Arquitetura web - Rest e Restful',
+    'Projeto - Store Manager'
+    , ,
+    'NodeJS - JWT - (JSON Web Token)',
+    'NodeJS - Upload de arquivos com Multer',
+    'Projeto - Cookmaster V2'
+    , ,
+    'Introdução - Deploy',
+    'Infraestrutura - Deploy com Heroku',
+    'Deploy - Gerenciadores de Processos',
+    'Projeto - Stranger Things'
+    , ,
+    'Projeto - Trybeer'
+    , ,
+    'Arquitetura - Princípios SOLID',
+    'ORM - Interface da aplicação com o banco de dados',
+    'ORM - Associations',
+    'Boas práticas na escrita de testes',
+    'Projeto - API de Blogs'
+    , ,
+    'Sockets - TCP/UDP & NET',
+    'Sockets - Socket.io',
+    'Projeto - Webchat'
+    , ,
+    'Projeto - Trybeer v2'
+    , ,
+    'Introdução - Ciência da Computação',
+    'Arquitetura de Computadores',
+    'Arquitetura de redes',
+    'Redes de computadores, ferramentas e segurança',
+    'Projeto - Explorando os protocolos'
+    , ,
+    'Aprendendo Python',
+    'Testes e Exceções',
+    'Entrada e Saída de dados',
+    'Raspagem de Dados',
+    'Projeto - Tech news'
+    , ,
+    'Introdução à programação orientada a objetos',
+    'Programação orientada a objetos na prática',
+    'Padrões de projeto',
+    'Projeto - Relatórios de Estoque'
+    , ,
+    'Estrutura de Dados I - Arrays',
+    'Estrutura de Dados I - Complexidade de Algoritmos',
+    'Recursividade e Estratégias para solução de problemas',
+    'Algoritmos de ordenação e busca',
+    'Projeto - Algoritmos'
+    , ,
+    'Estrutura de dados II - Hash maps & Dict',
+    'Estrutura de dados II - Set',
+    'Projeto - Restaurant Orders'
+    , ,
+    'Estrutura de dados III - Pilhas',
+    'Estrutura de dados III - Deque',
+    'Estrutura de Dados III - Nó & Listas ligadas',
+    'Estrutura de Dados III - Listas duplamente ligadas',
+    'Projeto - TING - Trybe Is Not Google']
